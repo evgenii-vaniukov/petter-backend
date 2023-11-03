133e1291f8e52cf391e35b334156f199ad433b0d
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-11-03 17:04:19 +0200

    Sets up a middleware and a route.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
+import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 import {signup} from "./features/auth/routes/signup.routes";
 
 const app = express();
+
+if (process.env.NODE_ENV == "dev") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi!");
+});
 
 app.use("/signup", signup);
 
