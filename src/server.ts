75b60245d145f7d67025a4f58c7335f05e727964
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-11-06 17:59:08 +0200

    .

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
@@ -8,9 +9,16 @@ const app = express();
 if (process.env.NODE_ENV == "dev") {
   app.use(morgan("dev"));
 }
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.send("Hi!");
+  res.json({message: "Hi!"});
 });
 
 app.use("/signup", signup);
