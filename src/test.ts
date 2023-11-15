8bd5b4b3a541a9ecef25479724abcc67a0178e97
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-11-15 11:41:54 +0200

    Adds vitest.

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs/promises";
-
-async function readPjson() {
-  const pjsonPath = "../package.json";
-  const pjson = await fs.readFile(pjsonPath, "utf-8");
-  const json = JSON.parse(pjson);
-
-  return json;
-}
